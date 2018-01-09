f8d653c2c0ec27d29951b329c3b47f20ae99c1dc
Nero <neroshan12@gmail.com>
2018-01-09 14:01:04 +0000

    Connect to mLab MongoDB database

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser'); // to handle reading data from <form>
 const app = express();
+const MongoClient = require('mongodb').MongoClient; //connect to MongoDB
+
+var db;
+MongoClient.connect(
+  'mongodb://<dbuser>:<dbpassword>@ds247327.mlab.com:47327/joblist',
+  (err, database) => {
+    if (err) return console.log(err);
+    db = database;
+    app.listen(3000, () => {
+      // moved into connect so servers only start when the database is connected
+      console.log('listening on 3000');
+    });
+  }
+);
 
 app.use(bodyParser.urlencoded({ extended: true })); // Express allows 'use' to add middleware to app
 // urlencoded withing bodyparser tell body parser to extract data from <form> element and add to the body property of request object
-app.listen(3000, () => {
-  console.log('listening on 3000');
-});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
