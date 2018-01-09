6bb6cfa2433cd1e444fa4865082bbe531d7a0cf6
Nero <neroshan12@gmail.com>
2018-01-09 12:01:47 +0000

    Add post request so it can be seen in the command line

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // to handle reading data from <form>
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true })); // Express allows 'use' to add middleware to app
+// urlencoded withing bodyparser tell body parser to extract data from <form> element and add to the body property of request object
 app.listen(3000, () => {
   console.log('listening on 3000');
 });
@@ -10,5 +13,5 @@ app.get('/', (req, res) => {
 });
 
 app.post('/joblist', (req, res) => {
-  console.log('Does this work?');
+  console.log(req.body);
 });
