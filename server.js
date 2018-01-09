83e394e0b2feae1b15235f7c67fb55496405a8c1
Nero <neroshan12@gmail.com>
2018-01-09 15:30:59 +0000

    Add functionality to allow database to return object

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var db;
 MongoClient.connect(
   'mongodb://<dbuser>:<dbpassword>@ds247327.mlab.com:47327/joblist',
   (err, client) => {
+    // MongoDB >= 3.0 you get a client object containing the database object in the callback
     if (err) return console.log(err);
     db = client.db('joblist');
 
@@ -22,7 +23,13 @@ app.use(bodyParser.urlencoded({ extended: true })); // Express allows 'use' to a
 // urlencoded withing bodyparser tell body parser to extract data from <form> element and add to the body property of request object
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  const cursor = db
+    .collection('jobslist')
+    .find()
+    .toArray((err, results) => {
+      console.log(results);
+      res.sendFile(__dirname + '/index.html');
+    });
 });
 
 app.post('/joblist', (req, res) => {
