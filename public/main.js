edb431748bd5af2cf02b17bb4359fd2387c2e1d3
Nero <neroshan12@gmail.com>
2018-01-10 17:01:23 +0000

    Complete Update feature

--- public/main.js
+++ public/main.js
@@ -1,16 +1,19 @@
 // external javascript file to execute the PUT request when the button is clicked
 app.use(express.static('public')); // built in middleware to make folder public
+app.use(bodyParser.json());
 
 var update = document.getElementById('update');
 
 update.addEventListener('click', () => {
   // send PUT request
   fetch('jobslist', {
+    method: 'put', // because sending a put request
+
+    headers: { 'Content-Type': 'application/json' }, // HTTP headers you want to send to the server (object with multiple K-V pairs). Content type = string converted to JSON obj.
     // fetch takes 2 param. (route) // and optional object that allows you to control different settings
     body: JSON.stringify({
       // body: content you send to the server
-      method: 'put', // because sending a put request
-      headers: { 'Content-Type': 'application/json' }, // HTTP headers you want to send to the server (object with multiple K-V pairs). Content type = string converted to JSON obj.
+
       company: 'Test',
       role: 'Test'
     })
