5bd864c6f9070ca1e5b7db44e3d171a765b91c18
Nero <neroshan12@gmail.com>
2018-01-10 13:46:18 +0000

    Add update button and public folder

--- public/main.js
+++ public/main.js
@@ -1,2 +1,11 @@
 // external javascript file to execute the PUT request when the button is clicked
+app.use(express.static('public')); // built in middleware to make folder public
+
+var update = document.getElementById('update');
+
+update.addEventListener('click', () => {
+  // send PUT request
+});
+
+// easiest way to trigger a PUT request is to use Fetch API
 
