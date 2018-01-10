7cf4ef0655b2e7da9d85e97658a640981e9c7ce0
Nero <neroshan12@gmail.com>
2018-01-10 14:16:27 +0000

    Add fetch for updating

--- public/main.js
+++ public/main.js
@@ -5,7 +5,16 @@ var update = document.getElementById('update');
 
 update.addEventListener('click', () => {
   // send PUT request
+  fetch('jobslist', {
+    // fetch takes 2 param. (route) // and optional object that allows you to control different settings
+    body: JSON.stringify({
+      // body: content you send to the server
+      method: 'put', // because sending a put request
+      headers: { 'Content-Type': 'application/json' }, // HTTP headers you want to send to the server (object with multiple K-V pairs). Content type = string converted to JSON obj.
+      company: 'Test',
+      role: 'Test'
+    })
+  });
 });
 
 // easiest way to trigger a PUT request is to use Fetch API
-
